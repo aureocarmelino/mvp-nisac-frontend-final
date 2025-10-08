c668edcaa3da60c4f4968879675a81ef8a16b721
Áureo Carmelino <aureo.roberto@hotmail.com>
2025-10-08 01:27:01 +0100

    Gerando build v4

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment =
 {
   production: false,
   baseUrl: 'https://mvp-nisac-backend-final.onrender.com',
-  tokenAllowedDomains: ["https://mvp-nisac-backend-final.onrender.com"],
+  tokenAllowedDomains: ["mvp-nisac-backend-final.onrender.com"],
   disallowedRoutes: [/\/oauth\/token/, /\/api\/auth\/signup/]
 };
 
